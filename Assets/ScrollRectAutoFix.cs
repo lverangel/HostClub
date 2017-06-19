60ba31f8f63375c8d15a30019c6c857696f28859
Hadrian <lverangel@gmail.com>
2017-06-19 15:15:31 +0800

    no message

---
+++ Assets/ScrollRectAutoFix.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollRectAutoFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int i = transform.childCount;
+		this.GetComponent<RectTransform>().sizeDelta = new Vector2(i*168 + (i-1)*8 + 16,240);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
